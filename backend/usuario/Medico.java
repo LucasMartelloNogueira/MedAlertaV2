109dd9231ffdb671b541024ddab1ecc679128b0c
valescamoura <valescamoura@id.uff.br>
2020-11-20 16:48:12 -0300

    adicionando classe agenda etc

--- backend/usuario/Medico.java
+++ backend/usuario/Medico.java
@@ -1,37 +1,43 @@
 package backend.usuario;
 
-import backend.Endereco;
 import backend.Pessoa;
 
 public class Medico extends Pessoa {
     private String especialidade;
-    private Endereco enderecoDaClinica;
 
-    //construtores
-    public Medico(String nome, String telefone, String email) {
-        super(nome, telefone, email);
-    }
-
-    public Medico(String nome, String telefone, String email, String especialidade, Endereco enderecoDaClinica) {
+    // construtor
+    public Medico(String nome, String telefone, String email, String especialidade) {
         super(nome, telefone, email);
         this.especialidade = especialidade;
-        this.enderecoDaClinica = enderecoDaClinica;
     }
 
-    //getters e setters
+    // get e set
     public String getEspecialidade() {
         return especialidade;
-    }
-
-    public Endereco getEnderecoDaClinica() {
-        return enderecoDaClinica;
     }
 
     public void setEspecialidade(String especialidade) {
         this.especialidade = especialidade;
     }
 
-    public void setEnderecoDaClinica(Endereco enderecoDaClinica) {
-        this.enderecoDaClinica = enderecoDaClinica;
+    // get e set particularidade
+    @Override
+    public Object getParticularidade() {
+        return getEspecialidade();
+    }
+
+    @Override
+    public <T> void setParticularidade(T novaParticularidade) {
+        setEspecialidade((String) novaParticularidade);
+    }
+
+    @Override
+    public int compareTo(Object outraPessoa) {
+        outraPessoa = (Pessoa) outraPessoa;
+        if(this.getNome().compareTo(outraPessoa.getNome()) > 0)
+            return 1;
+        if(this.getNome().compareTo(outraPessoa.getNome()) < 0)
+              return -1;
+        return 0;
     }
 }
--- backend/usuario/PessoaFisica.java
+++ backend/usuario/PessoaFisica.java
@@ -1,5 +1,36 @@
 package backend.usuario;
 
-public class PessoaFisica {
-    
+import backend.Agenda;
+import backend.Endereco;
+import backend.Pessoa;
+
+public class PessoaFisica extends Pessoa {
+    private Endereco endereco;
+    private Agenda contatosMedicos = new Agenda();
+    private Agenda constatosFarmacias = new Agenda();
+
+    public PessoaFisica(String nome, String telefone, String email, Endereco endereco) {
+        super(nome, telefone, email);
+        this.endereco = endereco;
+    }
+
+    //get e set
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    //get e set particularidade 
+    @Override
+    public Object getParticularidade() {
+        return getEndereco();
+    }
+
+    @Override
+    public <T> void setParticularidade(T novaParticularidade) {
+        setEndereco((Endereco)novaParticularidade);
+    }
 }
