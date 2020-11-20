7e1f024fbedc78c38a0a903c9fde0518ae666b56
Lucas Martello <lucas.martello0509@gmail.com>
2020-11-20 16:51:01 -0300

    leves mudancas

--- backend/farmacia/PessoaJuridica.java
+++ backend/farmacia/PessoaJuridica.java
@@ -1,5 +1,11 @@
 package backend.farmacia;
 
-public class PessoaJuridica {
+import backend.Pessoa;
+
+public class PessoaJuridica extends Pessoa{
     
+
+    public PessoaJuridica(String nome, String telefone, String email){
+        super(nome, telefone, email);
+    }
 }
